7043bf8b1859896ef162b8c96c9e7dcf2bb9ec49
Ines <ines.capela@infolabix.com>
2021-05-01 16:06:02 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Project_2021_PSRS
+<h3 align="center"> GO + GIN + JWT + Swagger + Hot reload (on Linux)</h3>
---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"net/http"
+
+	"api/model"
+	"api/routes"
+	"api/services"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+)
+
+var identityKey = "id"
+
+func init() {
+	services.OpenDatabase()
+	services.Db.AutoMigrate(&model.Users{})
+
+	defer services.Db.Close()
+}
+
+func main() {
+
+	services.FormatSwagger()
+
+	// Creates a gin router with default middleware:
+	// logger and recovery (crash-free) middleware
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.Use(gin.Recovery())
+
+	// Route not defined
+	router.NoRoute(func(c *gin.Context) {
+		c.JSON(404, gin.H{"code": "PAGE_NOT_FOUND", "message": "Page not found"})
+	})
+
+	// Routes without auth
+	router.GET("/echo/:echo", func(c *gin.Context) {
+		echo := c.Param("echo")
+
+		c.JSON(http.StatusOK, gin.H{
+			"echo": echo,
+		})
+	})
+
+	// Routes WITH auth
+	routesWithAuth := router.Group("/api/v1/")
+	routesWithAuth.Use(services.AuthorizationRequired())
+	{
+		// TODO: Put your routes here!
+	}
+
+	auth := router.Group("/api/v1/auth")
+	{
+		auth.POST("/login", routes.GenerateToken)
+		auth.POST("/register", routes.RegisterUser)
+		auth.PUT("/refresh_token", services.AuthorizationRequired(), routes.RefreshToken)
+	}
+
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	router.Run(":8080")
+}
